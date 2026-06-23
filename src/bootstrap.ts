c4f53dbf1cde16c7bf47413f80ea09292f75264d
Jomariel Gaitera <gaiterajomariel@gmail.com>
2026-06-23 20:05:04 +0800

    perf: lazy-load docs and move swagger/scalar to devDependencies

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,11 +1,10 @@
 import fastifyPlugin from 'fastify-plugin';
 import { autoload } from './plugins/autoload';
-import { docs } from './plugins/docs';
 import { env } from './packages/zod/env';
 
 export const bootstrap = fastifyPlugin(async function (app) {
   if (env('NODE_ENV') === 'development') {
-    await app.register(docs);
+    await app.register((await import('./plugins/docs.js')).docs);
   }
 
   await app.register(autoload);
